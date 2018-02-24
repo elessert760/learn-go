16028bf5d434add660e8e3ff47e2efa973f3101b
elessert760 <navydeltachi@gmail.com>
2018-02-23 16:05:10 -0800

    add arrays.go

---
+++ arrays.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+
+    var favNums2[5] float64
+
+    favNums2[0] = 3.14
+    favNums2[1] = 2.71828
+    favNums2[2] = 1.618
+    favNums2[3] = 304.0
+    favNums2[4] = 99
+
+    fmt.Println(favNums2[1])
+
+}
