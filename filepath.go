6c851e44c262a63e9974e030f3ceae1c0c6fc4a8
elessert760 <navydeltachi@gmail.com>
2018-03-06 13:48:42 -0800

    update filepath

--- filepath.go
+++ filepath.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "path/filepath"
+	"fmt"
+	"log"
+	"path/filepath"
 )
 
 func main() {
-    files, err := filepath.Glob("*.go")
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println(files)
-    fmt.Println(len(files)) // contains a list of all files in the current directory
+	files, err := filepath.Glob("*.go")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(files)
+	fmt.Println(len(files)) // contains a list of all files in the current directory
 }
